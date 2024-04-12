6cafe847b5f0ccfe4da5e556550fdc1a3acd1bff
Samaan Kamal <samaankamal72@gmail.com>
2024-04-12 21:43:48 +0200

    added form module to make it work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EventsComponent } from './events/events.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { EventFormComponent } from './event-form/event-form.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsComponent, HeaderComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, EventsComponent, HeaderComponent,EventFormComponent],
+  imports: [BrowserModule, FormsModule,HttpClientModule, AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
