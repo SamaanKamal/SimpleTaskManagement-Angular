aabb78ccfe3d040fd5289d796242646b775a16a6
Samaan Kamal <samaankamal72@gmail.com>
2024-04-12 16:57:45 +0200

    added event form component to gather user data for event

---
+++ src/app/event-form/event-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-form',
+  templateUrl: './event-form.component.html',
+  styleUrls: ['./event-form.component.css']
+})
+export class EventFormComponent {
+  isLoading = false;
+  error: string = null;
+}
