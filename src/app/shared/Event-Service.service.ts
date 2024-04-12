7fd5bb46cc665fb9c188c6eeeec729396b37aaf1
Samaan Kamal <samaankamal72@gmail.com>
2024-04-12 16:56:48 +0200

    implmented event crud opeartions in Service to handle http calls

--- src/app/shared/Event-Service.service.ts
+++ src/app/shared/Event-Service.service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, of } from 'rxjs';
+import { Event } from './Event.model';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
-export class EventService{
+export class EventService {
+  private apiUrl: string = '/api/Events';
+  private url: string = 'http://localhost:8080/';
+  constructor(private http: HttpClient) {}
 
+  fetchEvents() {
+    return this.http.get<Event[]>(this.url + this.apiUrl).pipe(
+      catchError((error) => {
+        console.error('Error fetching events:', error);
+        return of([]);
+      })
+    );
+  }
+  fetchEvent(eventId:number) {
+    return this.http.get<Event>(this.url + this.apiUrl + '/' +eventId ).pipe(
+      catchError((error) => {
+        console.error(
+          'Error fetching event: ',
+          error + ' with id : ' + eventId
+        );
+        return of([]);
+      })
+    );
+  }
+  createEvent(eventData:Event) {
+    return this.http.post<string>(`${this.url}/${this.apiUrl}/createEvent`, eventData).pipe(
+      catchError((error) => {
+        console.error('Error creating event:', error);
+        return of(null);
+      })
+    );
+  }
+    updateEvent(eventId:number,eventData:Event) {
+        return this.http.put<Event>(`${this.url}/${this.apiUrl}/updateEvent/${eventId}`, eventData)
+        .pipe(
+          catchError(error => {
+            console.error('Error updating event:', error + ' with id : ' + eventId);
+            return of(null);
+          })
+        );
+  }
+    deleteEvent(eventId:number) {
+        return this.http.delete<string>(`${this.url}/${this.apiUrl}/deleteEvent/${eventId}`)
+        .pipe(
+          catchError(error => {
+            console.error('Error deleting event:', error + ' with id : ' + eventId);
+            return of(false);
+          })
+        );
+  }
 }
