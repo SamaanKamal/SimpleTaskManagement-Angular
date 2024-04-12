b89bb4306268dcd0b21084a9c151787e761cf144
Samaan Kamal <samaankamal72@gmail.com>
2024-04-12 21:46:30 +0200

    changed to return type of observable to make it work

--- src/app/shared/Event-Service.service.ts
+++ src/app/shared/Event-Service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Event } from './Event.model';
 
 @Injectable({
@@ -8,19 +8,22 @@ import { Event } from './Event.model';
 })
 export class EventService {
   private apiUrl: string = '/api/Events';
-  private url: string = 'http://localhost:8080/';
+  private url: string = 'http://localhost:8080';
   constructor(private http: HttpClient) {}
 
-  fetchEvents() {
-    return this.http.get<Event[]>(this.url + this.apiUrl).pipe(
+  fetchEvents():Observable<any> {
+    return this.http.get<any>(this.url + this.apiUrl).pipe(
       catchError((error) => {
         console.error('Error fetching events:', error);
         return of([]);
       })
     );
   }
-  fetchEvent(eventId:number) {
-    return this.http.get<Event>(this.url + this.apiUrl + '/' +eventId ).pipe(
+  fetchEvent(eventId: number) {
+    return this.http.get<Event>(this.url + this.apiUrl + '/' + eventId).pipe(
+      map((events) => {
+        return events;
+      }),
       catchError((error) => {
         console.error(
           'Error fetching event: ',
@@ -30,30 +33,43 @@ export class EventService {
       })
     );
   }
-  createEvent(eventData:Event) {
-    return this.http.post<string>(`${this.url}/${this.apiUrl}/createEvent`, eventData).pipe(
-      catchError((error) => {
-        console.error('Error creating event:', error);
-        return of(null);
-      })
-    );
+  createEvent(eventData: Event) {
+    return this.http
+      .post<string>(`${this.url}${this.apiUrl}/createEvent`, eventData)
+      .pipe(
+        catchError((error) => {
+          console.error('Error creating event:', error);
+          return of(null);
+        })
+      );
   }
-    updateEvent(eventId:number,eventData:Event) {
-        return this.http.put<Event>(`${this.url}/${this.apiUrl}/updateEvent/${eventId}`, eventData)
-        .pipe(
-          catchError(error => {
-            console.error('Error updating event:', error + ' with id : ' + eventId);
-            return of(null);
-          })
-        );
+  updateEvent(eventId: number, eventData: Event) {
+    return this.http
+      .put<Event>(
+        `${this.url}/${this.apiUrl}/updateEvent/${eventId}`,
+        eventData
+      )
+      .pipe(
+        catchError((error) => {
+          console.error(
+            'Error updating event:',
+            error + ' with id : ' + eventId
+          );
+          return of(null);
+        })
+      );
   }
-    deleteEvent(eventId:number) {
-        return this.http.delete<string>(`${this.url}/${this.apiUrl}/deleteEvent/${eventId}`)
-        .pipe(
-          catchError(error => {
-            console.error('Error deleting event:', error + ' with id : ' + eventId);
-            return of(false);
-          })
-        );
+  deleteEvent(eventId: number) {
+    return this.http
+      .delete<string>(`${this.url}/${this.apiUrl}/deleteEvent/${eventId}`)
+      .pipe(
+        catchError((error) => {
+          console.error(
+            'Error deleting event:',
+            error + ' with id : ' + eventId
+          );
+          return of(false);
+        })
+      );
   }
 }
