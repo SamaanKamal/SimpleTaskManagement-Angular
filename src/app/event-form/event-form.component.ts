d0f2ac1fb16ed8716ccb4ea4abad79c4b24eb2f6
Samaan Kamal <samaankamal72@gmail.com>
2024-04-15 00:13:21 +0200

    added more fields to cover everything from the user

--- src/app/event-form/event-form.component.ts
+++ src/app/event-form/event-form.component.ts
@@ -13,6 +13,11 @@ export class EventFormComponent {
   isLoading = false;
   error: string = null;
   data: Event;
+  attendeeSelected: string = 'yes';
+  resourceSelected: string = 'yes';
+  optionalSelected: string = 'yes';
+  message: string = null;
+
 
   constructor(private eventService: EventService, private router: Router) {}
 
@@ -23,10 +28,9 @@ export class EventFormComponent {
     this.isLoading = true;
     const startTime = new Date(form.value.startTime);
     const endTime = new Date(form.value.endTime);
-    const isoString1 = startTime.toISOString(); // Convert to ISO 8601 string
+    const isoString1 = startTime.toISOString(); 
     const isoString2 = endTime.toISOString();
-    const timezoneOffset = '+02:00'; // Your desired timezone offset
-
+    const timezoneOffset = '+02:00'; 
     const formattedDate1 = isoString1.slice(0, 19) + timezoneOffset;
     const formattedDate2 = isoString2.slice(0, 19) + timezoneOffset;
     console.log(startTime);
@@ -51,9 +55,9 @@ export class EventFormComponent {
           responseStatus: form.value.responseStatus,
           comment: form.value.comment,
           additionalGuests: form.value.additionalGuests,
-          resource: true,
-          optional: false,
-          self: true,
+          resource: form.value.resourceSelected === 'yes' ? true:false,
+          optional: form.value.optionalSelected === 'yes' ? true:false,
+          self: form.value.attendeeSelected === 'yes' ? true:false,
         },
       ],
       attachments: [
@@ -69,8 +73,10 @@ export class EventFormComponent {
     this.eventService.createEvent(this.data).subscribe({
       next: (resonseData) => {
         console.log(resonseData);
-        this.router.navigate(['./events']);
+        this.message = resonseData;
+        this.eventService.setResponseMessage(this.message);
         this.isLoading = false;
+        this.router.navigate(['./created-Event']);
       },
       error: (errorMessage) => {
         console.log(errorMessage);
