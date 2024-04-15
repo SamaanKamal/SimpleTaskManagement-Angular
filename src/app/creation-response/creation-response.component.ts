0608244c8635f900c19d241c91a903c5b915ccd8
Samaan Kamal <samaankamal72@gmail.com>
2024-04-15 16:14:23 +0200

    solved the error  of creation and enhanced the look of the response

--- src/app/creation-response/creation-response.component.ts
+++ src/app/creation-response/creation-response.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EventService } from '../shared/Event-Service.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-creation-response',
   templateUrl: './creation-response.component.html',
-  styleUrls: ['./creation-response.component.css']
+  styleUrls: ['./creation-response.component.css'],
 })
-export class CreationResponseComponent implements OnInit,OnDestroy{
+export class CreationResponseComponent implements OnInit, OnDestroy {
   responseMessage: string;
   messageSubscription: Subscription;
+  success = false;
 
-  constructor(private eventService: EventService, private router: Router) { }
+  constructor(
+    private eventService: EventService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.messageSubscription =this.eventService.getResponseMessgage().subscribe((data) => {
-      this.responseMessage = data; 
+      this.responseMessage = data;
+      console.log(this.responseMessage)
+      this.success = true;
     });
   }
 
